33e295e356aa7bb071c65ef75a3b4a9ec3dbfc34
unknown <ukaran11@gmail.com>
2022-01-29 20:03:32 +0530

    Utsav | Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-     <HomePage />
+       <Routes>
+        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/" element={<HomePage />} />
+       </Routes>
     </div>
   );
 }
